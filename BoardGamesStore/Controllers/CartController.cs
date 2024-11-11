a95974d0913818770a94a7a2089c9704e6d9a75e
L10n42 <kola13r@gmail.com>
2024-11-11 07:29:55 +0200

    Version 9

--- BoardGamesStore/Controllers/CartController.cs
+++ BoardGamesStore/Controllers/CartController.cs
@@ -29,6 +29,7 @@ namespace BoardGamesStore.Controllers
 
             var cartItems = await _context.ShoppingCarts
                 .Include(c => c.BoardGame)
+                .ThenInclude(g => g.BoardGameImages)
                 .Where(c => c.UserID == userId)
                 .ToListAsync();
 
@@ -40,6 +41,81 @@ namespace BoardGamesStore.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> IncreaseQuantity(int id)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var cartItem = await _context.ShoppingCarts.FirstOrDefaultAsync(c => c.UserID == userId && c.BoardGameID == id);
+
+            if (cartItem != null)
+            {
+                cartItem.Quantity += 1;
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DecreaseQuantity(int id)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var cartItem = await _context.ShoppingCarts.FirstOrDefaultAsync(c => c.UserID == userId && c.BoardGameID == id);
+
+            if (cartItem != null)
+            {
+                if (cartItem.Quantity > 1)
+                {
+                    cartItem.Quantity -= 1;
+                }
+                else
+                {
+                    _context.ShoppingCarts.Remove(cartItem);
+                }
+
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Checkout(int id)
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var cartItem = await _context.ShoppingCarts.Include(c => c.BoardGame)
+                            .FirstOrDefaultAsync(c => c.UserID == userId && c.BoardGameID == id);
+
+            if (cartItem != null)
+            {
+                _context.ShoppingCarts.Remove(cartItem);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CheckoutAll()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var cartItems = await _context.ShoppingCarts.Include(c => c.BoardGame)
+                            .Where(c => c.UserID == userId).ToListAsync();
+
+            if (cartItems.Any())
+            {
+                _context.ShoppingCarts.RemoveRange(cartItems);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+
         }
 
         [HttpPost]
